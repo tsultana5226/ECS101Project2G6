76409a17e44383ea421af97854818df1cb6a2b1c
Garret W. Babick <71605834+garret-babick@users.noreply.github.com>
2020-10-20 11:00:02 -0400

    Update part3.py

--- part3.py
+++ part3.py
@@ -9,3 +9,17 @@ CA2=4
 CB1=5
 CB2=5
 Profit=0
+
+for i in range (1,1000001):
+
+    customer_loc=(random.random())*10
+
+    def score(location,price):
+        return (10-abs(customer_loc-location)+3*(6-price))
+
+    Score_A1= score(LA1,CA1)
+    Score_A2= score(LA2,CA2)
+    Score_B1= score(LB1,CB1)
+    Score_B2= score(LB2,CB2)
+
+    total_score= Score_A1 + Score_A2 + Score_B1 + Score_B2
